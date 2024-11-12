863869b9cf63fc3453e0e11a249256a66b4afd1e
zebu12 <jeanpierre.salum@outlook.com>
2024-11-12 23:27:24 +0100

    average with double

--- src/main/java/se/lexicon/GradeBook.java
+++ src/main/java/se/lexicon/GradeBook.java
@@ -26,20 +26,31 @@ public class GradeBook {
     public void determineAverageGrade() {
 
         int total = 0;
-        int counter = 1;
-        int average = 0;
-        int grade;
+        int counter = 0;
+        double average;
+        int grade = 0;
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Grade or -1 to exit: ");
+        grade = scanner.nextInt();
 
-        while (counter <= 10) {
-
-            System.out.print("Enter the Grade ");
-            grade = scanner.nextInt();
+        while (grade != -1) {
             total = total + grade;
             counter++;
+            System.out.print("Enter the Grade or -1 to exit: ");
+            grade = scanner.nextInt();
+
         }
-        average = total / counter;
-        System.out.println("The average grade is " + average);
-        System.out.println("The total grade is " + total);
+        if (counter != 0) {
+
+            average = (1.0 * total) / counter;
+            System.out.println("The total grade is " + total);
+            System.out.println("The average grade is " + average);
+
+
+        } else {
+
+            System.out.println("no grade were found");
+
+        }
     }
 }
